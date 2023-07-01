8e70ee02a310dc6048ada8897c6d23d6e5c058a5
GameWyrm <daronan97@hotmail.com>
2023-07-01 08:39:41 -0700

    Initial commit

---
+++ OuterWildsSummerJam/OuterWildsSummerJam.cs
@@ -0,0 +1,32 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OuterWildsSummerJam
+{
+    public class OuterWildsSummerJam : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsSummerJam)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+}
