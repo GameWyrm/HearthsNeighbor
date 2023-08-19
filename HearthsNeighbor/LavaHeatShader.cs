b6b5b74330207e72f41c1d8ab89b43ed269e2d6e
GameWyrm <daronan97@hotmail.com>
2023-08-19 08:42:10 -0700

    Added new shaders to Lava Core

---
+++ HearthsNeighbor/LavaHeatShader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace HearthsNeighbor
+{
+    public class LavaHeatShader : MonoBehaviour
+    {
+        private GameObject lavaCore;
+        private GameObject lavaObject;
+        private Material myMat;
+
+        private void Start()
+        {
+            lavaCore = HearthsNeighbor.Main.LavaPlanet;
+            lavaObject = lavaCore.transform.Find("Sector/MoltenCore").gameObject;
+            myMat = GetComponent<Renderer>().materials[0];
+        }
+
+        private void Update()
+        {
+            if (lavaCore != null && lavaObject != null)
+            {
+                myMat.SetVector("_BaseOrigin", lavaCore.transform.position);
+                myMat.SetFloat("_LavaHeight", lavaObject.transform.localScale.x);
+            }
+        }
+    }
+}
