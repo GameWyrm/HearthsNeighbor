238d4aba92c8f12225e88bb80227f39b42a49f92
GameWyrm <daronan97@hotmail.com>
2025-04-09 21:47:42 -0700

    Significant visual overhaul

---
+++ HearthsNeighbor/CopyRuleset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace HearthsNeighbor
+{
+    public class CopyRuleset : MonoBehaviour
+    {
+        private void Start ()
+        {
+            var myRuleset = GetComponent<EffectRuleset>();
+            var copyRuleset = HearthsNeighbor.Main.LakePlanet.transform.Find("Volumes/Ruleset").GetComponent<EffectRuleset>();
+
+
+        }
+    }
+}
---
+++ HearthsNeighbor/SetWaterVisibility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HearthsNeighbor
+{
+    public class SetWaterVisibility : MonoBehaviour
+    {
+        public bool enableWater = true;
+
+        private TessellatedSphereRenderer waterRenderer;
+
+        private void Start()
+        {
+            waterRenderer = HearthsNeighbor.Main.LakePlanet.transform.Find("Sector/Water").GetComponent<TessellatedSphereRenderer>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                waterRenderer.enabled = enableWater;
+            }
+        }
+    }
+}
